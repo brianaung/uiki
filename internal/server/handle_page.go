07e24a34dd04eaa08b502b0c6e1c7c49bf034f7f
Brian Aung <brianaung16@gmail.com>
2023-11-23 01:22:40 +1100

    implement landing page handler

--- internal/server/handle_page.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import "net/http"
-
-func (s *server) handleLanding() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	}
-}
